bffa15ecffe6bf5b313ed98b363ace7a2ccc506e
RomeoDespres <39157756+RomeoDespres@users.noreply.github.com>
2021-05-26 22:00:56 +0200

    Add Title component

---
+++ frontend/src/Title.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+import Fade from "@material-ui/core/Fade"
+import { makeStyles } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: theme.spacing(2),
+    },
+  },
+}))
+
+export default function Title({ title, subtitle }) {
+  const [showSubtitle, setShowSubtitle] = useState(false)
+
+  const classes = useStyles()
+
+  useEffect(() => {
+    setTimeout(() => setShowSubtitle(true), 500)
+  }, [])
+
+  return (
+    <>
+      <Fade in={true} timeout={750}>
+        <Typography variant="h1" className={classes.title}>
+          {title}
+        </Typography>
+      </Fade>
+      <Fade in={showSubtitle} timeout={750}>
+        <Typography variant="subtitle1">{subtitle}</Typography>
+      </Fade>
+    </>
+  )
+}
